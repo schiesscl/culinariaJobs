5a2be02e7c0eb29dc98339231b2088764e31958d
LeonardoEC <LeonardoDanielAV@proton.me>
2025-01-20 10:57:26 -0300

    Primera parte react

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -1,13 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Main from './main/Main'
+
 import './App.css'
 
 function App() {
 
   return (
     <>
-
+      <Main />
     </>
   )
 }
